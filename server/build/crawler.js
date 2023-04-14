18a1ae67487c5692eaa7cdf4745c0c0ba5737377
cglavin50 <cooperglavin@gmail.com>
2023-04-14 16:08:04 -0400

    Added a function to handle keywords. Likely will create a front-end to send back keywords (ex after a form). Next, should look calling this by groups of 25 for start position, until 999

--- server/build/crawler.js
+++ server/build/crawler.js
@@ -6,19 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios")); // calling methods, don't need to instantiate axios object
 const cheerio = require("cheerio");
 // import html = require('cheerio/lib/static');
-let url = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=software%20engineering%20intern&location=United%20StatesgeoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start=0";
-const getjobs = (html) => {
+let url = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=software%20engineering%20intern&location=United%20StatesgeoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start=0';
+const parsejobs = (html) => {
     const $ = cheerio.load(html);
     const jobsHTML = $('li'); // each job is a separate list item under linkedIn
     const jobs = [];
-    console.log(jobsHTML.length + " jobs found");
+    console.log(jobsHTML.length + ' jobs found');
     jobsHTML.each((index, element) => {
         const jobTitle = $(element).find('h3.base-search-card__title').text().trim();
         const company = $(element).find('h4.base-search-card__subtitle').text().trim();
         const companyPage = $(element).find('h4.base-search-card__subtitle').attr('href'); // attr's aren't guaranteed so can come out to undefined
         const location = $(element).find('span.job-search-card__location').text().trim();
         const link = $(element).find('a.base-card__full-link').attr('href');
-        const remote = jobTitle.includes("Remote") ? true : false;
+        const remote = jobTitle.includes('Remote') ? true : false;
         jobs.push({
             title: jobTitle,
             link: link,
@@ -33,6 +33,32 @@ const getjobs = (html) => {
 // fetch, then parse the jobs
 axios_1.default.get(url).then(response => {
     const html = response.data;
-    const jobs = getjobs(html); // API provides interface to interact with basically array of list items
+    const jobs = parsejobs(html); // API provides interface to interact with basically array of list items
     console.log(jobs);
 });
+const getJobs = (keywords, location) => {
+    // first, customize the url with keywords and location
+    let url = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords='; // =software%20engineering%20intern&location=United%20States'
+    let url2 = '&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start=0';
+    for (let i = 0; i < keywords.length; i++) {
+        const word = keywords[i];
+        const words = word.split(' ');
+        url += words[0];
+        for (let j = 1; j < words.length; j++) {
+            url += "%20" + words[j];
+        }
+    } // end for to append keywords
+    url += '&location=';
+    const locations = location.split(' ');
+    url += locations[0];
+    for (let i = 1; i < locations.length; i++) {
+        url += '%20' + locations[i];
+    }
+    url += url2;
+    // fetch, then parse the jobs
+    axios_1.default.get(url).then(response => {
+        const html = response.data;
+        const jobs = parsejobs(html); // API provides interface to interact with basically array of list items
+        console.log(jobs);
+    });
+};
--- server/src/crawler.ts
+++ server/src/crawler.ts
@@ -2,8 +2,8 @@ import axios from 'axios'; // calling methods, don't need to instantiate axios o
 import cheerio = require('cheerio');
 // import html = require('cheerio/lib/static');
 
-let url: string = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=software%20engineering%20intern&location=United%20StatesgeoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start=0";
-// ^ basic URL for swe intern, page number means nothing
+let url: string = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=software%20engineering%20intern&location=United%20StatesgeoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start=0';
+// ^ basic URL for swe intern, page number means nothing. 0 <= startnumber <= 999
 
 type Job = { // making all optional values as linkedIn posts are inconsistent
     title: string;
@@ -14,18 +14,18 @@ type Job = { // making all optional values as linkedIn posts are inconsistent
     companyPage?: string;
 }
 
-const getjobs = (html: string): Job[] => { // takes in html string and returns Cheerio interface with cheerio elements
+const parsejobs = (html: string): Job[] => { // takes in html string and returns Cheerio interface with cheerio elements
     const $:cheerio.CheerioAPI = cheerio.load(html);
     const jobsHTML: cheerio.Cheerio<cheerio.Element> = $('li'); // each job is a separate list item under linkedIn
     const jobs: Job[] = [];
-    console.log(jobsHTML.length + " jobs found");
+    console.log(jobsHTML.length + ' jobs found');
     jobsHTML.each((index, element) => {
         const jobTitle:string = $(element).find('h3.base-search-card__title').text().trim();
         const company:string = $(element).find('h4.base-search-card__subtitle').text().trim();
         const companyPage:string|undefined = $(element).find('h4.base-search-card__subtitle').attr('href'); // attr's aren't guaranteed so can come out to undefined
         const location:string = $(element).find('span.job-search-card__location').text().trim();
         const link:string|undefined = $(element).find('a.base-card__full-link').attr('href');
-        const remote: boolean = jobTitle.includes("Remote") ? true : false;
+        const remote: boolean = jobTitle.includes('Remote') ? true : false;
         jobs.push({
             title: jobTitle,
             link: link,
@@ -41,6 +41,36 @@ const getjobs = (html: string): Job[] => { // takes in html string and returns C
 // fetch, then parse the jobs
 axios.get(url).then( response => {
     const html: string = response.data;
-    const jobs: Job[] = getjobs(html); // API provides interface to interact with basically array of list items
+    const jobs: Job[] = parsejobs(html); // API provides interface to interact with basically array of list items
     console.log(jobs);
 });
+
+const getJobs = (keywords: string[], location: string) => {
+    // first, customize the url with keywords and location
+    let url: string = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords='; // =software%20engineering%20intern&location=United%20States'
+    let url2: string = '&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start=0';
+    for (let i: number = 0; i < keywords.length; i++)
+    {
+        const word: string = keywords[i];
+        const words:string[] = word.split(' ');
+        url += words[0];
+        for (let j: number = 1; j < words.length; j++)
+        {
+            url += "%20" + words[j];
+        }
+    } // end for to append keywords
+    url += '&location=';
+    const locations: string[] = location.split(' ');
+    url += locations[0];
+    for (let i: number = 1; i < locations.length; i++)
+    {
+        url += '%20' + locations[i];
+    }
+    url += url2;
+    // fetch, then parse the jobs
+    axios.get(url).then( response => {
+    const html: string = response.data;
+    const jobs: Job[] = parsejobs(html); // API provides interface to interact with basically array of list items
+    console.log(jobs);
+    });
+}
